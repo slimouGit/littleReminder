38f5be7e4941b25cdb5383b7ea87f359b56e8010
Salim Oussayfi <box@slimou.net>
2017-08-02 20:54:07 +0200

    ...

--- script/remind.php
+++ script/remind.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>littleReminder</title>
-    <link rel="stylesheet" href="layout/layout.css">
+    <link rel="stylesheet" href="../layout/layout.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <div id="littleReminderForm">
-        <a href="index.html"><img class="success" src="img/littleReminderSuccess.png"/></a>
+        <a href="../index.html"><img class="success" src="../img/littleReminderSuccess.png"/></a>
     </div>
 </body>
 </html>
